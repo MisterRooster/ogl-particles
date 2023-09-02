1679d07890275cd2d7ffd942c0cc6338c830dde7
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-09-02 19:28:31 +0200

    Updated Readme: preview header refresh

--- src/thirdparty/ImGuiBuild.cpp
+++ src/thirdparty/ImGuiBuild.cpp
@@ -1,3 +1,11 @@
+/*------------------------------------------------------------------------------------------------*\
+| ogl-particles
+|
+| Copyright (c) 2023 MisterRooster (github.com/MisterRooster). All rights reserved.
+| Licensed under the MIT license. See LICENSE file for full terms.
+| This notice is not to be removed.
+\*------------------------------------------------------------------------------------------------*/
+
 #include "imgui.cpp"
 #include "imgui_draw.cpp"
 #include "imgui_tables.cpp"
--- src/thirdparty/oglp_imgui_config.h
+++ src/thirdparty/oglp_imgui_config.h
@@ -1,3 +1,10 @@
+/*------------------------------------------------------------------------------------------------*\
+| ogl-particles
+|
+| Copyright (c) 2023 MisterRooster (github.com/MisterRooster). All rights reserved.
+| Licensed under the MIT license. See LICENSE file for full terms.
+| This notice is not to be removed.
+\*------------------------------------------------------------------------------------------------*/
 #pragma once
 
 #define IMGUI_USE_WCHAR32			// Use 32-bit for ImWchar (default is 16-bit) to support unicode planes 1-16.
