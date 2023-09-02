27ad9b1f3c113580c410adf1bcae3c10ec7adbe3
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-09-02 17:07:50 +0200

    Added support for unicode planes 1-16

---
+++ src/thirdparty/oglp_imgui_config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define IMGUI_USE_WCHAR32			// Use 32-bit for ImWchar (default is 16-bit) to support unicode planes 1-16.
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+
+#define IMGUI_DISABLE_DEMO_WINDOWS	// Disable demo windows: ShowDemoWindow()/ShowStyleEditor() will be empty.
+#define IMGUI_DISABLE_DEBUG_TOOLS	// Disable metrics/debugger and other debug tools: ShowMetricsWindow(), ShowDebugLogWindow() and ShowStackToolWindow() will be empty
