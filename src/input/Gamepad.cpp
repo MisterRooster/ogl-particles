b6602d20fd36c80be6d231ae13a30db045445c93
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2026-07-16 22:54:37 +0200

    Remove redundant XInput pragma comment
    - #pragma comment(lib, "Xinput9_1_0.lib") is an MSVC-only directive that
      GCC reports as an unknown pragma
    - It is also redundant: CMake already links Xinput9_1_0.lib explicitly for
      Windows builds, which is what made the GCC toolchain work in the first
      place

--- src/input/Gamepad.cpp
+++ src/input/Gamepad.cpp
@@ -5,8 +5,6 @@
 | Licensed under the MIT license. See LICENSE file for full terms.
 | This notice is not to be removed.
 \*------------------------------------------------------------------------------------------------*/
-#pragma comment(lib, "Xinput9_1_0.lib")
-
 #include "Gamepad.h"
 #include "utility/Debug.h"
 #include <Xinput.h>
